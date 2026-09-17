1cbd2230a8c7c63eeed7eaa7535c4372e2094b05
0118Add <60157610+0118Add@users.noreply.github.com>
2026-09-17 13:36:13 +0800

    Update curl command and git_sparse_clone for packages
    Uncommented the curl command to fetch 29_ports.js and updated the git_sparse_clone command for default-settings.

--- scripts/immortalwrt.sh
+++ scripts/immortalwrt.sh
@@ -146,7 +146,7 @@ git clone --depth=1 -b main https://github.com/sirpdboy/luci-app-partexp package
 
 # 替换autocore default-settings
 git clone --depth=1 -b openwrt-25.12 https://github.com/sbwml/autocore-arm package/autocore
-git_sparse_clone master https://github.com/8688Add/openwrt_pkgs default-settings
+#git_sparse_clone master https://github.com/8688Add/openwrt_pkgs default-settings
 
 # 添加 rtp2httpd
 #git_sparse_clone https://github.com/stackia/rtp2httpd/tree/main/openwrt-support/luci-app-rtp2httpd
@@ -165,7 +165,7 @@ sed -i 's/procd_set_param stderr 1/procd_set_param stderr 0/g' feeds/packages/ut
 
 sed -i 's/解除网易云音乐播放限制/音乐解锁/g' feeds/luci/applications/luci-app-unblockneteasemusic/root/usr/share/luci/menu.d/luci-app-unblockneteasemusic.json
 curl -fsSL https://raw.githubusercontent.com/0118Add/X86_64-Test/main/general/25_storage.js > ./feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/25_storage.js
-#curl -fsSL https://raw.githubusercontent.com/immortalwrt/luci/master/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/29_ports.js > ./package/autocore/files/generic/29_ports.js
+curl -fsSL https://raw.githubusercontent.com/immortalwrt/luci/master/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/29_ports.js > ./package/autocore/files/generic/29_ports.js
 # 7. 其他
 # 专门针对 advancedplus 的流氓逻辑进行清洗
 #if [ -f package/luci-app-advancedplus/root/etc/init.d/advancedplus ]; then
